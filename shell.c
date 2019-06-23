7e150c0c12ba340fac1be93805b9901fe668d184
DJM199708 <47919778+DJM199708@users.noreply.github.com>
2019-06-23 11:29:08 +0800

    DJM
    add func "backstage running" ; change layout of main;

--- shell.c
+++ shell.c
@@ -1,4 +1,4 @@
-﻿/*********************
+/*********************
 *Project: TUBShell
 *Description: a private shell for OS command by C
 *Author: DJM ZHJ WQ
@@ -41,21 +41,25 @@ for(p = 1; p < M; p ++){
 }*/
 
 /****** 函数声明 ******/
-void sayHello();        //进入提示
-void printPrefix();     //打印段前缀 依赖getprompt_wq()
-void pipe_zhj();		//管道通信
-void getprompt_wq();	//获取用户信息
-void cd_wq();			//cd 命令
-void ls_djm();			//ls 命令
-void touch_djm();		//touch 命令
-void gedit_djm();		//gedit 命令
+void sayHello();        					//进入提示
+void printPrefix();     					//打印段前缀 依赖getprompt_wq()
+void getprompt_wq();						//获取用户信息
+int cd_wq(char op_path[2][MAX_CMD_LEN/2]);	//cd 命令
+int ls_djm();								//ls 命令
+int touch_djm(char filename[]);				//touch 命令
+int gedit_djm(char filename[]);				//gedit 命令
+int commonCmd_execError_search();			//命令不能exec外部调用，去查找我们做的命令实现
+int commonCmd_djm();						//常规命令
+int pipeCmd_zhj();							// | 管道命令
+int redirectCmd_djm();						//< > 重定向命令
+int backstageCmd_djm();						//& 后台处理命令
 void commandStrSplit(char contentStr[]);	//对命令字符串分割
 void commandStrLinkAndSplit();				//对commandCompose[]连接重分割
-int commandJudge();						//对命令判断是否为特殊cmd(& | < >)
+int commandJudge();							//对命令判断是否为特殊cmd(& | < >)
 int commandControl();						//对命令判断应调用什么函数完成
 char *alias(char commanddStr[]);			//别名替换
-int getInputCommand();  //获取输入命令
-void shell();           //shell的总入口
+int getInputCommand();  					//获取输入命令
+void shell();           					//shell的总入口
 
 /****** 函数实现 ******/
 /***** Info *****/
@@ -86,17 +90,12 @@ void printPrefix(){
 	host = store_promptGet("hostname");
 	folder = store_promptGet("pathname");
 	root = store_promptGet("root");
-	printf("[TUBShell %s@%s:%s]%s ",user, host, folder, root);
+	printf("[\e[36;1mTUBShell\e[0m \e[32;1m%s@%s\e[0m:\e[34;1m%s\e[0m]%s ",user, host, folder, root);
 	//free(user);
 	//free(host);
 	//free(folder);
 	//free(root);
 }
-
-/***** Info *****/
-/* Author: ZHJ */
-/* Function: 实现管道*/
-void pipe_zhj(){}
 
 /***** Info *****/
 /* Author: WQ */
@@ -137,28 +136,40 @@ void getprompt_wq(){
 /***** Info *****/
 /* Author: WQ */
 /* Function: cd功能 */
-void cd_wq(){
+int cd_wq(char op_path[2][MAX_CMD_LEN/2]){
 	/* Note:放在能调用存储命令数组commandCompose[]同一文件里*/
+	/* 调用成功返回1，失败返回-1 */
+	int ret = 1;
 	struct passwd *pwd;
-	char pathname[MAX_PATH_LEN];			//储存路径名
-	pwd = getpwuid(getuid());				//获取用户信息 
-	if(strcmp(commandCompose[1]," ") == 0){	//如果第二个字符串为空，则代表进入根目录 
-		strcpy(pathname, pwd->pw_dir);		//获取pathname   pwd->pw_dir获取的目录都是/root 
-		if(chdir(pathname) == -1){			//如果有错
-			perror("myshell: chdir");		//报错
+	char pathname[MAX_PATH_LEN];					//储存路径名
+	pwd = getpwuid(getuid());						//获取用户信息 
+	
+	//打印测试
+	printf("op is %s\n", op_path[0]);
+	printf("path is %s\n", op_path[1]);
+	
+	if(strcmp(op_path[0],"") == 0){			//如果第二个字符串为空，则代表进入根目录 
+		strcpy(pathname, pwd->pw_dir);				//获取pathname   pwd->pw_dir获取的目录都是/root 
+		if((ret = chdir(pathname)) == -1){			//如果有错
+			perror("TUBshell: chdir");				//报错
 			exit(1);
 		}
-	} else {								//如果有路径
-		if(chdir(commandCompose[1]) == -1){	//如果chdir执行失败
+	} else {										//如果有路径
+		if((ret = chdir(op_path[1])) == -1){		//如果chdir执行失败
 			printf("TUBshell: cd: %s :No such file or directory\n", commandCompose[1]);	
 		}	
 	}
+	
+	if(!ret)	//ret = 0; chdir() success
+		ret = 1; 
+	printf("cd_wq ret is %d\n", ret);
+	return ret;
 }
 
 /***** Info *****/
 /* Author: DJM */
 /* Function: ls功能 */
-void ls_djm(){
+int ls_djm(){
 	//权限 硬链接数 拥有者 所属组 文件大小 创建时间 文件名
 	/* struct stat TUBstat;
 	if(lstat(filename, &TUBstat) == -1){
@@ -185,24 +196,26 @@ void ls_djm(){
 	}
 	printf("\n");
 	closedir(dir);
+	return 1;
 }
 
 /***** Info *****/
 /* Author: DJM */
 /* Function: touch功能 */
-void touch_djm(char filename[]){
+int touch_djm(char filename[]){
 	int fd = -1;
 	fd = open(filename, O_WRONLY|O_CREAT,MODE_RW_UGO);
 	//write(fd, s, sizeof(s));
 	if(fd == -1)
 		printf("create file error\n");
 	close(fd);
+	return 1;
 }
 
 /***** Info *****/
 /* Author: DJM */
 /* Function: gedit功能 */
-void gedit_djm(char filename[]){
+int gedit_djm(char filename[]){
 	if(fork() == 0){
 		if(execlp("gedit", "gedit", filename, NULL) == -1){
 			perror("execl \'gedit\' error\n");
@@ -210,6 +223,97 @@ void gedit_djm(char filename[]){
 		}
 	}
 	sleep(1);
+	return 1;
+}
+
+/***** Info *****/
+/* Author: DJM */
+/* Function: 查找我们做的命令实现 */
+int commonCmd_execError_search(){
+	/* Note: 命令不能exec外部调用时查找 */
+	int ret = -1;
+	char str_Op_Path[2][MAX_CMD_LEN / 2];
+	strcpy(str_Op_Path[0], commandCompose[0]);
+	strcpy(str_Op_Path[1], commandCompose[1]);
+	ret = (strcmp(str_Op_Path[0], "cd")) ? ret : cd_wq(str_Op_Path);
+	ret = (strcmp(str_Op_Path[0], "touch")) ? ret : touch_djm("none");
+	ret = (strcmp(str_Op_Path[0], "gedit")) ? ret : gedit_djm("none");
+	return ret;
+}
+
+/***** Info *****/
+/* Author: DJM */
+/* Function: 实现常规命令 */
+int commonCmd_djm(){
+	int __switch = 0;		/* 常规命令标识为0的返回 */
+	int funcRet = -1;		/* 用于函数内部获取调用函数返回值 1/-1 success/failure */
+	char cmd[MAX_CMD_LEN];
+	strcpy(cmd ,commandCompose[0]);
+	if(vfork() == 0){
+		if((__switch = execvp(cmd, commandCompose)) == -1){
+			//perror("execvp error\n");
+			/* 因为子进程中调用函数会出现数据乱码 */
+			/* 故对自己实现的命令只能在此进行控制调用 */
+			/* Note: 常规命令exec无法调用，查找程序中有无实现 */
+			
+			//printf("cd path is %s\n", commandCompose[0]);
+			//printf("cd path is %s\n", commandCompose[1]);
+			//printf("cd path is %s\n", commandCompose[2]);
+			/*
+			char str_Op[MAX_CMD_LEN/2];
+			char str_Path[MAX_CMD_LEN/2];
+			strcpy(str_Op, commandCompose[0]);
+			strcpy(str_Path, commandCompose[1]);
+			funcRet = (strcmp(cmd, "cd")) ? funcRet : cd_wq(str_Op, str_Path);
+			funcRet = (strcmp(cmd, "touch")) ? funcRet : touch_djm("none");
+			funcRet = (strcmp(cmd, "gedit")) ? funcRet : gedit_djm("none");
+			*/
+			funcRet = commonCmd_execError_search();
+			if(funcRet == 1){	//查询成功
+				__switch = 0;
+			} else {			//查询失败
+				printf("Command \'%s\' not found\n", cmd);
+			}
+			exit(1);
+		}
+	}
+	sleep(1);
+	return __switch;
+};
+
+/***** Info *****/
+/* Author: ZHJ */
+/* Function: 实现管道命令 */
+int pipeCmd_zhj(){}
+
+/***** Info *****/
+/* Author: DJM */
+/* Function: 实现重定向命令 */
+int redirectCmd_djm(){}
+
+/***** Info *****/
+/* Author: DJM */
+/* Function: 实现后台处理命令 */
+int backstageCmd_djm(){
+	int i= -1;
+	int pid;
+	int __switch = 3;
+	char cmd[MAX_CMD_LEN];
+	strcpy(cmd ,commandCompose[0]);
+	
+	while(strcmp(commandCompose[++i], "&"));
+	commandCompose[i] = NULL;
+	
+	if((pid = fork()) == 0){
+		sleep(1);
+		printf("\n");
+		if((execvp(cmd, commandCompose)) == -1){
+			perror("execvp error\n");
+			exit(1);
+		}
+	}
+	printf("Pid [\e[32;1m%d\e[0m]\n", pid);
+	return __switch;
 }
 
 /***** Info *****/
@@ -227,13 +331,13 @@ void commandStrSplit(char * contentStr){
 			continue;
 		} else if (contentStr[j] == ' ' && strlen(strtmp[k]) != 0){
 			commandCompose[i++] = strtmp[k++];
-		} else if(contentStr[j] == '|' || contentStr[j] == '>' || contentStr[j] == '<'){
+		} else if(contentStr[j]=='|'||contentStr[j]=='>'||contentStr[j]=='<'||contentStr[j]=='&'){
 			if(strlen(strtmp[k]) != 0){
 				commandCompose[i++] = strtmp[k++];
 			}
 			strtmp[k][0] = contentStr[j];
 			commandCompose[i++] = strtmp[k++];
-		} else if(contentStr[j] != ' '){
+		} else if(contentStr[j] != ' ' && contentStr[j] != '\n' ){
 			for(m = 0; strtmp[k][m]; m++){}
 			strtmp[k][m] = contentStr[j];
 			
@@ -242,8 +346,7 @@ void commandStrSplit(char * contentStr){
 				break;
 			}
 		}
-		
-		if(contentStr[j] != ' ' || contentStr[j] != '\n')
+		if(contentStr[j] != ' ' || contentStr[j] != '\n' || contentStr[j] != '\0')
 			len++;
 	}
 	commandCompose[i] = NULL;
@@ -313,26 +416,23 @@ int commandJudge(){
 /* Author: DJM */
 /* Function: 命令控制器 */
 int commandControl(int __switch){
-	/* Note: __switch 为 0/1/2/3 分别代表 常规/管道/重定向/后台控制  命令 */
+	/* Note: __switch 为 0/1/2/3 分别代表 常规/管道/重定向/后台控制 命令 */
 	char cmd[MAX_CMD_LEN];
 	strcpy(cmd ,commandCompose[0]);
 	
 	/* 判断命令调用 */
 	switch(__switch){
 		case 0:
-			if(fork() == 0){
-				//if((execlp("ls", "ls", NULL)) == -1){
-				if((execvp(cmd, commandCompose)) == -1){
-					perror("execvp error\n");
-					exit(1);
-				}
-			}
-			sleep(1);
+			__switch = commonCmd_djm();
 			break;
-		case 1: ;
-		case 2: ;
-		case 3: 
+		case 1:
 			printf("command %s is done\n", cmd);
+			break;
+		case 2:
+			printf("command %s is done\n", cmd);
+			break;
+		case 3: 
+			__switch = backstageCmd_djm();
 			break;
 		default: printf("command \'%s\' no exist\n", cmd);;
 	}
@@ -371,7 +471,9 @@ int getInputCommand(){
 	
 	__switch = commandJudge();
 	__switch = commandControl(__switch);	/* exec command */
-	return (__switch+1);
+	
+	/* 因四种命令分别为 0 1 2 3 ，故+1使得继续运行TUBShell */
+	return (__switch + 1);
 }
 
 /***** Info *****/
